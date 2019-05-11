9579bc2f79b8f82196bad53d937ab8eaddb3ba02
Sakib Hasan Nishat <39929993+sakib1207@users.noreply.github.com>
2019-05-11 21:24:10 +0600

    Update CaeserCipher.py

--- CryptGraphyCodes/CaeserCipher.py
+++ CryptGraphyCodes/CaeserCipher.py
@@ -1,18 +1,19 @@
-small_letter='abcdefghijklmnopqrstuvwxyz'
-capital_letter='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-newmessage=''
-newmessage2=''
-key=int(input('key value: '))
-message=input('Please enter a message for encrypt your message as the value of key: ')
+#for encrypted and decrypted process follow code and also the comments line
+small_letter='abcdefghijklmnopqrstuvwxyz'#REMAIN SAME
+capital_letter='ABCDEFGHIJKLMNOPQRSTUVWXYZ'#REMAIN SAME
+newmessage=''#REMAIN SAME
+newmessage2=''#REMAIN SAME
+key=int(input('key value: '))#REMAIN SAME
+message=input('Please enter a message for encrypt your message as the value of key: ')#REMAIN SAME
 for character in message:
      if character in small_letter:
          position=small_letter.find(character)
-         newposition=(position-key) % 26
+         newposition=(position-key) % 26#CHANGE HERE + WILL BE -
          newcharacter=small_letter[newposition] 
          newmessage =newmessage+newcharacter
      elif character in capital_letter:
          position=capital_letter.find(character)
-         newposition=(position-key) % 26
+         newposition=(position-key) % 26#CHANGE HERE + WILL BE -
          newcharacter=capital_letter[newposition]
          newmessage =newmessage+newcharacter
      else:
